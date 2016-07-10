45862340686c32760fee604ba4512a2c62cd2514
Wapaul1 <william11235@gmail.com>
2016-07-10 19:58:14 +0000

    Initial Version of Alexnet with Ray

--- examples/imagenet_cnn/driver.py
+++ examples/imagenet_cnn/driver.py
@@ -3,7 +3,6 @@ import ray
 import ray.services as services
 import os
 import tensorflow as tf
-
 import functions
 
 def oneandzeros(x):
@@ -12,21 +11,20 @@ def oneandzeros(x):
   return(zero)
 
 num_workers = 3
+batchnum = 0
+weights = []
 
 worker_dir = os.path.dirname(os.path.abspath(__file__))
 worker_path = os.path.join(worker_dir, "worker.py")
 services.start_ray_local(num_workers=num_workers, worker_path=worker_path)
 
-batchnum = 0
-Y = np.asarray(map(oneandzeros,np.random.randint(0, 1000, size=[200])))
-X = np.random.uniform(size=[200,224,224,3])
-xref = ray.put(X)
-yref = ray.put(Y)
-weights = []
+
+Y = np.asarray(map(oneandzeros,np.random.randint(0, 1000, size=[1000]))) # Random labels and images in lieu of actual imagenet data
+X = np.random.uniform(size=[1000, 224, 224, 3])
 for placeholder in functions.placeholders:
   weights.append(np.random.normal(scale = 1e-1, size=placeholder.get_shape()))
 print "weights inited"
-functions.sess.run(functions.assignment, feed_dict=dict(zip(functions.placeholders,weights)))
+functions.sess.run(functions.assignment, feed_dict=dict(zip(functions.placeholders, weights)))
 print "Weights passed"
 while True:
   results = []
@@ -39,16 +37,18 @@ while True:
     functions.updateweights(*weightrefs)
   print "Workers updated"
   for i in range(num_workers):
-    print "Sending stuff"
-    results.append(functions.computegrad(xref,yref))
+    randindices = np.random.randint(0, 1000, size=[20])
+    xref = ray.put(np.asarray(map(lambda(i): X[i], randindices)))
+    yref = ray.put(np.asarray(map(lambda(i): Y[i], randindices)))
+    results.append(functions.computegrad(xref, yref))
   print "Grads recieved"
-  actualresult = map(lambda(x):map(ray.get,x), results)
-  print map(len, actualresult)
-  grads = [np.asarray([gradset[i] for gradset in actualresult]) for i in range(16)]
-  print grads[0].shape
-  gradientvalues = map(lambda(x):tf.convert_to_tensor(np.mean(x,axis=0)), grads)
-  functions.sess.run(functions.opt.apply_gradients(zip(gradientvalues,functions.parameters)))
-  if (batchnum == 100):
-    print functions.accuracy.eval(feed_dict={images:X, y_true:Y, dropout:0.0})
+  actualresult = map(lambda(x): map(ray.get, x), results)
+  grads = [np.asarray([gradset[i] for gradset in actualresult]) for i in range(16)] # 16 gradients, one for each variable
+  gradientvalues = map(lambda(x): np.mean(x, axis=0), grads) # Taking mean over all the samples
+  functions.sess.run(functions.application, feed_dict=dict(zip(functions.placeholders, gradientvalues))) # Feeding the new values in
+  if (batchnum % 10 == 0):
+    functions.printaccuracy(X, Y)
+  print("End of batch {}".format(batchnum))
+  batchnum += 1
 
 services.cleanup()
--- examples/imagenet_cnn/functions.py
+++ examples/imagenet_cnn/functions.py
@@ -1,19 +1,18 @@
-# Most of the tensorflow code is adapted from Tensorflow's tutorial on using CNNs to train MNIST
-# https://www.tensorflow.org/versions/r0.9/tutorials/mnist/pros/index.html#build-a-multilayer-convolutional-network
 import tensorflow as tf
 import numpy as np
 import ray
-@ray.remote(16*[np.ndarray],[])
+
+@ray.remote(16 * [np.ndarray], [])
 def updateweights(*weight):
-  print "Weights run"
-  feed_dict = dict(zip(placeholders,weight))
+  feed_dict = dict(zip(placeholders, weight))
   sess.run(assignment, feed_dict=feed_dict)
 
-@ray.remote([np.ndarray, np.ndarray], 16*[np.ndarray])
+@ray.remote([np.ndarray, np.ndarray], 16 * [np.ndarray])
 def computegrad(X, Y):
-  print "Computing gradient"
   return sess.run([g for (g,v) in compgrads], feed_dict={images:X, y_true:Y, dropout:0.5})
 
+def printaccuracy(X, Y):
+    print sess.run(accuracy, feed_dict={images:X, y_true:Y, dropout:1.0})
 
 images = tf.placeholder(tf.float32, shape=[None, 224, 224, 3])
 y_true = tf.placeholder(tf.float32, shape=[None, 1000])
@@ -22,7 +21,7 @@ assignment = []
 placeholders = []
 # conv1
 with tf.name_scope('conv1') as scope:
-  kernel = tf.Variable(tf.truncated_normal([11,11,3,96], dtype=tf.float32, stddev=1e-1))
+  kernel = tf.Variable(tf.truncated_normal([11, 11, 3, 96], dtype=tf.float32, stddev=1e-1))
   conv = tf.nn.conv2d(images, kernel, [1, 4, 4, 1], padding='SAME')
   biases = tf.Variable(tf.constant(0.0, shape=[96], dtype=tf.float32),
                        trainable=True, name='biases')
@@ -30,8 +29,8 @@ with tf.name_scope('conv1') as scope:
   conv1 = tf.nn.relu(bias, name=scope)
   parameters += [kernel, biases]
 
-  kernel_new=tf.placeholder(tf.float32, shape=kernel.get_shape())
-  biases_new=tf.placeholder(tf.float32, shape=biases.get_shape())
+  kernel_new = tf.placeholder(tf.float32, shape=kernel.get_shape())
+  biases_new = tf.placeholder(tf.float32, shape=biases.get_shape())
   update_kernel = kernel.assign(kernel_new)
   update_biases = biases.assign(biases_new)
   assignment += [update_kernel, update_biases]
@@ -59,8 +58,8 @@ with tf.name_scope('conv2') as scope:
   conv2 = tf.nn.relu(bias, name=scope)
   parameters += [kernel, biases]
 
-  kernel_new=tf.placeholder(tf.float32, shape=kernel.get_shape())
-  biases_new=tf.placeholder(tf.float32, shape=biases.get_shape())
+  kernel_new = tf.placeholder(tf.float32, shape=kernel.get_shape())
+  biases_new = tf.placeholder(tf.float32, shape=biases.get_shape())
   update_kernel = kernel.assign(kernel_new)
   update_biases = biases.assign(biases_new)
   assignment += [update_kernel, update_biases]
@@ -87,8 +86,8 @@ with tf.name_scope('conv3') as scope:
   conv3 = tf.nn.relu(bias, name=scope)
   parameters += [kernel, biases]
 
-  kernel_new=tf.placeholder(tf.float32, shape=kernel.get_shape())
-  biases_new=tf.placeholder(tf.float32, shape=biases.get_shape())
+  kernel_new = tf.placeholder(tf.float32, shape=kernel.get_shape())
+  biases_new = tf.placeholder(tf.float32, shape=biases.get_shape())
   update_kernel = kernel.assign(kernel_new)
   update_biases = biases.assign(biases_new)
   assignment += [update_kernel, update_biases]
@@ -104,8 +103,8 @@ with tf.name_scope('conv4') as scope:
   bias = tf.nn.bias_add(conv, biases)
   conv4 = tf.nn.relu(bias, name=scope)
   parameters += [kernel, biases]
-  kernel_new=tf.placeholder(tf.float32, shape=kernel.get_shape())
-  biases_new=tf.placeholder(tf.float32, shape=biases.get_shape())
+  kernel_new = tf.placeholder(tf.float32, shape=kernel.get_shape())
+  biases_new = tf.placeholder(tf.float32, shape=biases.get_shape())
   update_kernel = kernel.assign(kernel_new)
   update_biases = biases.assign(biases_new)
   assignment += [update_kernel, update_biases]
@@ -121,8 +120,8 @@ with tf.name_scope('conv5') as scope:
   bias = tf.nn.bias_add(conv, biases)
   conv5 = tf.nn.relu(bias, name=scope)
   parameters += [kernel, biases]
-  kernel_new=tf.placeholder(tf.float32, shape=kernel.get_shape())
-  biases_new=tf.placeholder(tf.float32, shape=biases.get_shape())
+  kernel_new = tf.placeholder(tf.float32, shape=kernel.get_shape())
+  biases_new = tf.placeholder(tf.float32, shape=biases.get_shape())
   update_kernel = kernel.assign(kernel_new)
   update_biases = biases.assign(biases_new)
   assignment += [update_kernel, update_biases]
@@ -140,6 +139,7 @@ pool5lrn = tf.nn.lrn(pool5, depth_radius=5, bias=1.0,
                             alpha=0.0001, beta=0.75,
                             name="LocalResponseNormalization")
 dropout = tf.placeholder(tf.float32)
+
 with tf.name_scope('fc1') as scope:
   n_input = int(np.prod(pool5lrn.get_shape().as_list()[1:]))
   kernel = tf.Variable(tf.truncated_normal([n_input, 4096], dtype=tf.float32,
@@ -150,12 +150,13 @@ with tf.name_scope('fc1') as scope:
   fc_layer1 = tf.nn.tanh(tf.nn.bias_add(tf.matmul(fc_in, kernel), biases))
   fc_out1 = tf.nn.dropout(fc_layer1, dropout)
   parameters += [kernel, biases]
-  kernel_new=tf.placeholder(tf.float32, shape=kernel.get_shape())
-  biases_new=tf.placeholder(tf.float32, shape=biases.get_shape())
+  kernel_new = tf.placeholder(tf.float32, shape=kernel.get_shape())
+  biases_new = tf.placeholder(tf.float32, shape=biases.get_shape())
   update_kernel = kernel.assign(kernel_new)
   update_biases = biases.assign(biases_new)
   assignment += [update_kernel, update_biases]
   placeholders += [kernel_new, biases_new]
+
 with tf.name_scope('fc2') as scope:
   n_input = int(np.prod(fc_out1.get_shape().as_list()[1:]))
   kernel = tf.Variable(tf.truncated_normal([n_input, 4096], dtype=tf.float32,
@@ -166,8 +167,8 @@ with tf.name_scope('fc2') as scope:
   fc_layer2 = tf.nn.tanh(tf.nn.bias_add(tf.matmul(fc_in, kernel), biases))
   fc_out2 = tf.nn.dropout(fc_layer2, dropout)
   parameters += [kernel, biases]
-  kernel_new=tf.placeholder(tf.float32, shape=kernel.get_shape())
-  biases_new=tf.placeholder(tf.float32, shape=biases.get_shape())
+  kernel_new = tf.placeholder(tf.float32, shape=kernel.get_shape())
+  biases_new = tf.placeholder(tf.float32, shape=biases.get_shape())
   update_kernel = kernel.assign(kernel_new)
   update_biases = biases.assign(biases_new)
   assignment += [update_kernel, update_biases]
@@ -182,25 +183,28 @@ with tf.name_scope('fc3') as scope:
                          trainable=True, name='biases')
   fc_layer3 = tf.nn.softmax(tf.nn.bias_add(tf.matmul(fc_in, kernel), biases))
   parameters += [kernel, biases]
-  kernel_new=tf.placeholder(tf.float32, shape=kernel.get_shape())
-  biases_new=tf.placeholder(tf.float32, shape=biases.get_shape())
+  kernel_new = tf.placeholder(tf.float32, shape=kernel.get_shape())
+  biases_new = tf.placeholder(tf.float32, shape=biases.get_shape())
   update_kernel = kernel.assign(kernel_new)
   update_biases = biases.assign(biases_new)
   assignment += [update_kernel, update_biases]
   placeholders += [kernel_new, biases_new]
 
 y_pred = fc_layer3 / tf.reduce_sum(fc_layer3,
-                        reduction_indices=len(fc_layer3.get_shape())-1,
+                        reduction_indices=len(fc_layer3.get_shape()) - 1,
                         keep_dims=True)
 # manual computation of crossentropy
 y_pred = tf.clip_by_value(y_pred, tf.cast(1e-10, dtype=tf.float32),
-                          tf.cast(1.-1e-10, dtype=tf.float32))
+                          tf.cast(1. - 1e-10, dtype=tf.float32))
 cross_entropy = tf.reduce_mean(-tf.reduce_sum(y_true * tf.log(y_pred),
-                                reduction_indices=len(y_pred.get_shape())-1))
-opt =  tf.train.MomentumOptimizer(learning_rate=0.001, momentum=0.9)
+                                reduction_indices=len(y_pred.get_shape()) - 1))
+#opt =  tf.train.MomentumOptimizer(learning_rate=0.001, momentum=0.9)
+opt = tf.train.MomentumOptimizer(learning_rate=0.001, momentum=0.9) # aNY OTHER OPTIMIZIER CAN BE PLACED HERE
 correct_pred = tf.equal(tf.argmax(y_pred, 1), tf.argmax(y_true, 1))
 accuracy = tf.reduce_mean(tf.cast(correct_pred, tf.float32))
 
 compgrads = opt.compute_gradients(cross_entropy, parameters)
+
+application = opt.apply_gradients(zip(placeholders,parameters))
 sess = tf.Session()
 sess.run(tf.initialize_all_variables())
