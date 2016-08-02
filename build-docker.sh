a1a8556644ed4dd0fb690db23058808976b2ba7e
Johann Schleier-Smith <jssmith@gmail.com>
2016-08-02 14:41:06 -0700

    restore non-test docker builds

--- build-docker.sh
+++ build-docker.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-docker build -t amplab/ray:base docker/base
-docker build -t amplab/ray:examples-base docker/examples-base
 docker build -t amplab/ray:devel docker/devel
 docker build -t amplab/ray:deploy docker/deploy
 docker build -t amplab/ray:examples docker/examples
