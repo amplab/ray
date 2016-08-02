d723fb631ac3542ba5e0c6f4836ecce1905eb740
Johann Schleier-Smith <jssmith@gmail.com>
2016-08-02 15:42:48 -0700

    add missing file

--- test/travis-ci/hyperopt_example.sh
+++ test/travis-ci/hyperopt_example.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# Hyperoptimization test
+# Runs only on Docker under Linux
 if [[ $TRAVIS_OS_NAME == 'linux' ]]; then
   docker run --shm-size=500m amplab/ray:test-examples bash -c 'source setup-env.sh && cd examples/hyperopt && python driver.py'
 fi
